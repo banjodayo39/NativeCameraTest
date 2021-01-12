1cdf886d35bd2ba5f8752cf2d467138648773938
Banjo Dayo <banjodayo39@gmail.com>
2021-01-12 22:37:32 +0100

    add texture view

--- app/src/main/java/com/banjodayo/nativecameratest/opengl/AirHockeyRenderer.java
+++ app/src/main/java/com/banjodayo/nativecameratest/opengl/AirHockeyRenderer.java
@@ -2,14 +2,19 @@ package com.banjodayo.nativecameratest.opengl;
 
 import android.content.Context;
 
-import android.opengl.GLES20;
-import android.opengl.GLES30;
+
 import android.opengl.GLSurfaceView;
 
 import com.banjodayo.nativecameratest.R;
+import com.banjodayo.nativecameratest.objects.Mallet;
+import com.banjodayo.nativecameratest.objects.Table;
+import com.banjodayo.nativecameratest.programs.ColorShaderProgram;
+import com.banjodayo.nativecameratest.programs.TextureShaderProgram;
 import com.banjodayo.nativecameratest.utils.LoggerConfig;
+import com.banjodayo.nativecameratest.utils.MatrixHelper;
 import com.banjodayo.nativecameratest.utils.ShaderHelper;
 import com.banjodayo.nativecameratest.utils.TextResourcesReader;
+import com.banjodayo.nativecameratest.utils.TextureHelper;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -19,132 +24,78 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import static android.opengl.GLES20.*;
+import static android.opengl.Matrix.multiplyMM;
+import static android.opengl.Matrix.rotateM;
+import static android.opengl.Matrix.setIdentityM;
+import static android.opengl.Matrix.translateM;
 
 public class AirHockeyRenderer implements GLSurfaceView.Renderer {
 
     private final Context context;
-
-    private static final int POSITION_COMPONENT_COUNT = 2;
-    private static final int BYTES_PER_FLOAT = 4;
-    private final FloatBuffer vertexData;
-
-    private int program;
-
-    private static final String U_COLOR = "u_Color";
-    private int uColorLocation;
-
-    private static final String A_POSITION = "a_Position";
-    private int aPositionLocation;
-
-    private static final String A_COLOR = "a_Color";
-    private static final int COLOR_COMPONENT_COUNT = 3;
-    private static final int STRIDE =
-            (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT;
-
-    private int aColorLocation;
+    private final float[] projectionMatrix = new float[16];
+    private final float[] modelMatrix = new float[16];
+    private Table table;
+    private Mallet mallet;
+    private TextureShaderProgram textureProgram;
+    private ColorShaderProgram colorProgram;
+    private int texture;
 
     public AirHockeyRenderer(Context context) {
         this.context = context;
-
-        float[] tableVertices = {
-                0f, 0f,
-                0f, 14f,
-                9f, 14f,
-                9f, 0f
-        };
-
-        float[] tableVerticesWithTriangles = {
-                // Order of coordinates: X, Y, R, G, B
-                // Triangle Fan
-                0f, 0f, 1f, 1f, 1f,
-
-                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
-                0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
-                0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
-                -0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
-                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
-
-                // Line 1
-                -0.5f, 0f, 1f, 0f, 0f,
-                0.5f, 0f, 1f, 0f, 0f,
-
-                // Mallets
-                0f, -0.25f, 0f, 0f, 1f,
-                0f, 0.25f, 1f, 0f, 0f
-        };
-
-        vertexData = ByteBuffer
-                .allocateDirect(tableVerticesWithTriangles.length * BYTES_PER_FLOAT)
-                .order(ByteOrder.nativeOrder())
-                .asFloatBuffer();
-        vertexData.put(tableVerticesWithTriangles);
     }
 
     @Override
-    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
-        //glClearColor(1f, 0, 0, 1f);
+    public void onSurfaceCreated(GL10 glUnused, EGLConfig config) {
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-
-        String vertexShaderSource = TextResourcesReader
-                .readTextFileFromResource(context, R.raw.simple_vertex_shader);
-        String fragmentShaderSource = TextResourcesReader
-                .readTextFileFromResource(context, R.raw.simple_fragment_shader);
-
-        int vertexShader = ShaderHelper.compileVertexShader(vertexShaderSource);
-        int fragmentShader = ShaderHelper.compileFragmentShader(fragmentShaderSource);
-
-        //link the vertex and the fragment shader together
-        program = ShaderHelper.linkProgram(vertexShader, fragmentShader);
-
-        if (LoggerConfig.ON) {
-            ShaderHelper.validateProgram(program);
-        }
-
-        // use the program
-        glUseProgram(program);
-
-        //get the  uniform location
-        aColorLocation = glGetAttribLocation(program, A_COLOR);
-
-        //get the attribute location
-        aPositionLocation = glGetAttribLocation(program, A_POSITION);
-
-        //associating an array of vertex with the shader
-        vertexData.position(0);
-        glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT,
-                false, STRIDE, vertexData);
-
-        //enabling the vertex array
-        glEnableVertexAttribArray(aPositionLocation);
-
-        vertexData.position(POSITION_COMPONENT_COUNT);
-        glVertexAttribPointer(aColorLocation, COLOR_COMPONENT_COUNT, GL_FLOAT,
-                false, STRIDE, vertexData);
-        glEnableVertexAttribArray(aColorLocation);
+        table = new Table();
+        mallet = new Mallet();
+        textureProgram = new TextureShaderProgram(context);
+        colorProgram = new ColorShaderProgram(context);
+        texture = TextureHelper.loadTexture(context, R.drawable.air_hockey_surface);
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl10, int width, int height) {
         glViewport(0, 0, width  , height);
+
+        MatrixHelper.perspectiveM(projectionMatrix, 45, (float) width
+                / (float) height, 1f, 10f);
+
+        setIdentityM(modelMatrix, 0);
+        translateM(modelMatrix, 0, 0f, 0f, -2.5f);
+        rotateM(modelMatrix, 0, -60f, 1f, 0f, 0f);
+
+        final float[] temp = new float[16];
+        multiplyMM(temp, 0, projectionMatrix, 0, modelMatrix, 0);
+        System.arraycopy(temp, 0, projectionMatrix, 0, temp.length);
+
+//        // screen rotation
+//        final float aspectRatio = width > height ? (float) width / (float) height : (float) height / (float) width;
+//        if (width > height) {
+//        // Landscape
+//            orthoM(projectionMatrix, 0, -aspectRatio, aspectRatio, -1f, 1f, -1f, 1f);
+//        } else {
+//        // Portrait or square
+//            orthoM(projectionMatrix, 0, -1f, 1f, -aspectRatio, aspectRatio, -1f, 1f);
+//        }
     }
-    
+
 
     @Override
-    public void onDrawFrame(GL10 gl10) {
+    public void onDrawFrame(GL10 glUnused) {
+        // Clear the rendering surface.
         glClear(GL_COLOR_BUFFER_BIT);
 
-       // glUniform4f(uColorLocation, 1.0f, 1.0f, 1.0f, 1.0f);
-        glDrawArrays(GL_TRIANGLE_FAN, 0, 6);
+        // Draw the table.
+        textureProgram.useProgram();
+        textureProgram.setUniforms(projectionMatrix, texture);
+        table.bindData(textureProgram);
+        table.draw();
 
-       // glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
-        glDrawArrays(GL_LINES, 6, 2);
-
-        // Draw the first mallet blue.
-       // glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
-        glDrawArrays(GL_POINTS, 8, 1);
-
-        // Draw the second mallet red.
-        //glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
-        glDrawArrays(GL_POINTS, 9, 1);
+        // Draw the mallets.
+        colorProgram.useProgram();
+        colorProgram.setUniforms(projectionMatrix);
+        mallet.bindData(colorProgram);
+        mallet.draw();
     }
 }
